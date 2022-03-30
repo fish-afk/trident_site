d09d478e8fa269e176c5e01b9e65911c0b69e2fa
Shihab <mirzashihab2@gmail.com>
2022-03-30 07:55:34 +0200

    Update firebase.js

--- src/components/Account/firebase.js
+++ src/components/Account/firebase.js
@@ -6,13 +6,7 @@ import { getAuth, onAuthStateChanged } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCSveaJQ3poDlpinQMqYQikLQZYJpzXKRE",
-  authDomain: "trident-jewelerry.firebaseapp.com",
-  projectId: "trident-jewelerry",
-  storageBucket: "trident-jewelerry.appspot.com",
-  messagingSenderId: "136865192928",
-  appId: "1:136865192928:web:594acc3bebb4d2ae746985",
-  measurementId: "G-96YX0F9F0G"
+  "secret": "secret"
 };
 
 // Initialize Firebase
